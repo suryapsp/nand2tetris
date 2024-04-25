a6de1a539f766fed070f2022909b801af93fd09e
suryapsp <techie-human@protonmail.com>
2024-04-25 20:12:32 +0530

    implemented multiplication(mult.asm)

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,4 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+@R2
+M = 0
+@R1
+D = M 
+@i 
+M = D
+
+(LOOP)
+    @i
+    D = M
+    @END
+    D;JEQ
+
+    @R0
+    D = M
+
+    @R2
+    M = M+D
+
+    @i
+    M = M-1
+
+    @LOOP 
+    0;JMP 
+
+(END)
+    @END
+    0;JMP
+    
